8901e5a2d5b044e0fe652ed120cac226cf1cdf07
Rubi <agustin.n.rubinstein@gmail.com>
2025-01-21 14:07:01 -0300

    made unit converter, hasn't been tested

--- commands/utility/Convert.js
+++ commands/utility/Convert.js
@@ -1,10 +1,30 @@
 const {SlashCommandBuilder} = require ('discord.js');
 const { execute } = require('./Ping');
 
+//Make the units converter
+function  temperatureConverter_FtoC (temperature){ 
+    convertedTemp = temperature * (5/9) + 32;
+
+    return convertedTemp
+}
+
+
+//Make the command
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('Convert')
-    .setDescription('Converts F to C'),
+    .setDescription('Converts F to C')
+    .addNumberOption(Option => 
+        Option.setName('F')
+        .setDescription('temperature to convert in burger units')
+        .setRequired(true)
 
-    async execute (interaction){}
+    ),
+
+    async execute (interaction){
+        const value = interaction.options.getNumber('F')
+        const newValue = temperatureConverter_FtoC(value)
+        await interaction.reply(`${value} is ${newValue} in real units`)
+
+    }
 }
